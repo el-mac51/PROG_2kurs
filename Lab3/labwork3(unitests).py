ba1fd3f854354ad0f95a6278c225cf2a30850467
el-mickk51 <stormtornado16@gmail.com>
2025-11-17 17:47:46 +0300

    Update labwork3(unitests).py
    поменяно было все что можно было поменять

--- Lab3/labwork3(unitests).py
+++ Lab3/labwork3(unitests).py
@@ -1,67 +1,52 @@
 import unittest
-
-class TreeNode: 
-    def __init__(self, value):
-        self.value = value
-        self.left = None
-        self.right = None
-
-def tree(root, max_height, current_height = 1): #основной код дерева
-    if current_height > max_height:
-        return None
-    node = TreeNode(root)
-    if current_height < max_height:
-        node.left = tree(root * 4, max_height, current_height + 1)
-        node.right = tree(root + 1, max_height, current_height + 1)
-    return node
-
-my_tree = tree(root = 4, max_height = 4)
+from labwork3 import gen_bin_tree
 
 class TestTree(unittest.TestCase):
 
     def test_creation(self):
-
-        root = tree(4,1)
+        """прверяем что дерево создается"""
+        root = gen_bin_tree(height=1, root=4)
         self.assertIsNotNone(root)
-        self.assertEqual(root.value, 4)
-        self.assertIsNone(root.left)
-        self.assertIsNone(root.right)
+        self.assertEqual(root[0], 4)
+        self.assertIsNone(root[1])
+        self.assertIsNone(root[2])
 
-    def test_height_2(self): #проверям дерево высотой 2
-
-        root = tree(4, 2) #это высота 1
-        self.assertEqual(root.value, 4) #проверяем корень дерева
+    def test_height_2(self):
+        """проверяем два уровня дерева"""
+        root = gen_bin_tree(height=2, root=4)
+        self.assertEqual(root[0], 4)
         
-        self.assertIsNotNone(root.left) #это высота 2
-        self.assertEqual(root.left.value, 16)
-        self.assertIsNotNone(root.right)
-        self.assertEqual(root.right.value, 5)
+        self.assertIsNotNone(root[1])
+        self.assertEqual(root[1][0], 16)
+        self.assertIsNotNone(root[2])
+        self.assertEqual(root[2][0], 5)
 
-        self.assertIsNone(root.left.left) #проевряем что после 2й высоты ничего
-        self.assertIsNone(root.left.right)
-        self.assertIsNone(root.right.left)
-        self.assertIsNone(root.right.right)
+        self.assertIsNone(root[1][1])
+        self.assertIsNone(root[1][2])
+        self.assertIsNone(root[2][1])
+        self.assertIsNone(root[2][2])
 
-    def test_edges(self): #проверяем что на нулевой и отрицательной высотах ничего
-        self.assertIsNone(tree(4, 0))
-        self.assertIsNone(tree(4, -1))
+    def test_edges(self):
+        """проверяем крайние значения с нулевым и отрицательным значениями"""
+        self.assertIsNone(gen_bin_tree(height=0, root=4))
+        self.assertIsNone(gen_bin_tree(height=-1, root=4))
 
     def test_formula(self):
-        root = tree(10, 2)        
-        self.assertEqual(root.left.value, 40)  #проверяем формулы для левых и правых узлов
-        self.assertEqual(root.right.value, 11) 
+        """проверяем формулы для узлов с обоих сторон"""
+        root = gen_bin_tree(height=2, root=10)        
+        self.assertEqual(root[1][0], 40)
+        self.assertEqual(root[2][0], 11)
 
     def test_node_count(self):
-
-        def count_nodes(node): #считаем узлы
-
-            if node is None: # если узлов нет
+        """проверяем количество узлов в дереве"""
+        def count_nodes(node):
+            if node is None:
                 return 0
-            return 1 + count_nodes(node.left) + count_nodes(node.right)
+            return 1 + count_nodes(node[1]) + count_nodes(node[2])
     
-        root = tree(4, 4) #проверяем для наших значений дерева
+        root = gen_bin_tree(height=4, root=4)
         nodes = count_nodes(root)
-        self.assertEqual(nodes, 15) #в дереве высотой 4 должно быть 15 узлов
-        
+        self.assertEqual(nodes, 15)
+
 if __name__ == "__main__":
     unittest.main()
